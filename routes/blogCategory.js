604387eee38b588d404eb0a366580259a5db0d09
Aliac <alimehdizadeh822@gmail.com>
2024-07-21 13:56:58 +0330

    feat :sparkles: : Blog Category CRUD completed

--- routes/blogCategory.js
+++ routes/blogCategory.js
@@ -2,21 +2,32 @@ const express = require("express");
 const router = express.Router();
 const BlogCategory = require("../models/BlogCategory");
 const { body, validationResult } = require("express-validator");
+const BlogPost = require("../models/BlogPost");
 
 // Read all categories
 router.get("/blog-categories", async (req, res) => {
   try {
-    const categories = await BlogCategory.find().sort({ name: 1 });
-    res.render("pages/admin/blog_categories", { title:"Blog Categories",categories });
+    const blogCategories = await BlogCategory.find().sort({ name: 1 });
+    // Use Promise.all to wait for all usageCount promises to resolve
+    const categoriesWithUsageCount = await Promise.all(
+      blogCategories.map(async (category) => {
+        const usageCount = await BlogPost.find({
+          category: category._id,
+        }).countDocuments();
+        return {
+          ...category.toObject(), // Convert Mongoose document to plain object
+          usageCount,
+        };
+      })
+    );
+    res.render("pages/admin/blog_categories", {
+      title: "Blog Categories",
+      blogCategories: categoriesWithUsageCount,
+    });
   } catch (err) {
     console.log(err);
     res.redirect("/admin");
   }
-});
-
-// Create a new category
-router.get("/create-blog-category", (req, res) => {
-  res.render("admin/createCategory", { errors: [] });
 });
 
 router.post(
@@ -25,33 +36,28 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render("admin/createCategory", { errors: errors.array() });
+      req.flash("error", errors.array());
+      return res.redirect("/admin/blog-categories");
     }
 
     try {
       const { name } = req.body;
+      if (BlogCategory.findOne({name:name})) {
+        req.flash("error", ["Blog Category With This Name Exists"]);
+        return res.redirect("/admin/blog-categories");
+      }
 
       const category = new BlogCategory({ name });
       await category.save();
-
-      res.redirect("/admin/categories");
+      req.flash("success", ["Blog Category Created Successfully"]);
+      res.redirect("/admin/blog-categories");
     } catch (err) {
       console.log(err);
-      res.render("admin/createCategory", { errors: [{ msg: err.message }] });
+      req.flash("error", ["Unknown Error"]);
+      return res.redirect("/admin/blog-categories");
     }
   }
 );
-
-// Edit a category
-router.get("/edit-blog-category/:id", async (req, res) => {
-  try {
-    const category = await BlogCategory.findById(req.params.id);
-    res.render("admin/editCategory", { category, errors: [] });
-  } catch (err) {
-    console.log(err);
-    res.redirect("/admin/categories");
-  }
-});
 
 router.post(
   "/edit-blog-category/:id",
@@ -59,21 +65,20 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const category = { _id: req.params.id, ...req.body };
-      return res.render("admin/editCategory", {
-        category,
-        errors: errors.array(),
-      });
+      req.flash("error", errors.array());
+      return res.redirect("/admin/blog-categories");
     }
 
     try {
       const { name } = req.body;
 
       await BlogCategory.findByIdAndUpdate(req.params.id, { name });
-      res.redirect("/admin/categories");
+      req.flash("success", ["Blog Category Updated Successfully"]);
+      return res.redirect("/admin/blog-categories");
     } catch (err) {
       console.log(err);
-      res.redirect("/admin/categories");
+      req.flash("error", ["Unknown Error"]);
+      return res.redirect("/admin/blog-categories");
     }
   }
 );
@@ -82,10 +87,12 @@ router.post(
 router.post("/delete-blog-category/:id", async (req, res) => {
   try {
     await BlogCategory.findByIdAndDelete(req.params.id);
-    res.redirect("/admin/categories");
+    req.flash("success", ["Blog Category Deleted Successfully"]);
+    return res.redirect("/admin/blog-categories");
   } catch (err) {
     console.log(err);
-    res.redirect("/admin/categories");
+    req.flash("error", ["Unknown Error"]);
+    return res.redirect("/admin/blog-categories");
   }
 });
 
